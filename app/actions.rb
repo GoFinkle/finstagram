411a8b542a1891b470096a526a19ce7532ce3f3e
GoFinkle <andrewtcharbonneau@gmail.com>
2022-02-05 13:47:20 +0000

    W2D2

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,12 @@
 get '/' do
     File.read(File.join('app/views', 'index.html'))
 end
+
+# When a browser requests the root of the application
+get '/' do
+
+    #sens the string "Hello World!" to the browser
+    "Hello world!"
+
+#Stop
+end
