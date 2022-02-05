f206c6ea931a4080c779dd2c490c3874431a55cb
GoFinkle <andrewtcharbonneau@gmail.com>
2022-02-05 14:01:22 +0000

    W2D2

--- app/actions.rb
+++ app/actions.rb
@@ -1,12 +1,20 @@
 get '/' do
     File.read(File.join('app/views', 'index.html'))
+    
+    username = "sharky_j"
+    avatar_url = "http://naserca.com/images/sharky_j.jpg"
+    photo_url = "http://naserca.com/images/shark.jpg"
+    time_ago_in_minutes = 15
+    like_count = 0
+    comment_count = 1
+    comments = [
+        "sharky_j: Out for the long weekend... too embarassed to show y'all the beach bod!"
+    ]
+
+
 end
 
 # When a browser requests the root of the application
-get '/' do
 
-    #sens the string "Hello World!" to the browser
-    "Hello world!"
 
-#Stop
-end
+#sends the string to the browser
