f3739fd842e4cbf3b976ae670f0225fe0329929e
GoFinkle <andrewtcharbonneau@gmail.com>
2022-02-18 20:42:27 +0000

    W4D2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
 
-    user = User.new({ username: "sharky_j", avatar_url: "http://naserca.com/images/sharky_j.jpg" })
+    #user = User.new({ username: "sharky_j", avatar_url: "http://naserca.com/images/sharky_j.jpg" })
+    has_many :finstagram_posts
+    has_many :comments
+    has_many :likes
 
 end
