e523c4d6519d11d502d6ecc5064ab9f49139cf55
GoFinkle <andrewtcharbonneau@gmail.com>
2022-02-20 15:24:11 +0000

    W5D1A2

--- app/actions.rb
+++ app/actions.rb
@@ -27,16 +27,22 @@ get '/signup' do     # if a user navigates to the path "/signup",
     erb(:login)      # render app/views/login.erb
   end
 
-  post '/login' do    # when we submit a form with an action of /login
+  post '/login' do
     username = params[:username]
     password = params[:password]
-
-    # 1. find user by username
-    user = User.find_by(username: username)
-
-    if user && user.password == password
-      "Success!"
+  
+    @user = User.find_by(username: username)
+  
+    if @user && @user.password == password
+      session[:user_id] = @user.id
+      "Success! User with id #{session[:user_id]} is logged in!"
     else
-      "Login failed."
+      @error_message = "Login failed."
+      erb(:login)
     end
+  end
+
+  get '/logout' do
+    session[:user_id] = nil
+    "Logout successful!"
   end
